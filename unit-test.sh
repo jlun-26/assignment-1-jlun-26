b0657bbe8d9842f0e78b39b131750aeedd544522
jlun-26 <junlun26@gmail.com>
2026-03-02 02:51:37 +0800

    Initial environment setup and fix build error

--- unit-test.sh
+++ unit-test.sh
@@ -4,8 +4,8 @@
 # Automate these steps from the readme:
 # Create a build subdirectory, change into it, run
 # cmake .. && make && run the assignment-autotest application
-mkdir -p build
-cd build
+mkdir -p ./build
+cd ./build
 cmake ..
 make clean
 make
